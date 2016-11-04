c275bc6aff6ccdbabc9ba363df91d4bb9ffb4ff2
Dimiter Jilanov <d.jilanov@mdg.bg>
2016-11-04 19:11:02 +0200

    Integrate base detail iamge func

--- src/app/components/zoomable_images/zoomable_images.component.ts
+++ src/app/components/zoomable_images/zoomable_images.component.ts
@@ -10,17 +10,6 @@ export class ZoomableImagesComponent {
     
     @Input()
     zoomableImages: Array<Object>;
-
-    private imagesArray: Array<Object> = [{
-        image: `//placekitten.com/1599/300`,
-        text: `test1`
-      },{
-        image: `//placekitten.com/1598/300`,
-        text: `test2`
-      },{
-        image: `//placekitten.com/1597/300`,
-        text: `test3`
-      }];
     
     constructor() {
         
