395e28e3157744917160ceab2c39f42573dd0af2
Dimitar Jilanov <dimitar.jilanov@bulpros.com>
2017-11-22 14:24:21 +0200

    Fix wrong path of server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '/../dist')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '../dist/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
 
 app.listen(8080, function () {
