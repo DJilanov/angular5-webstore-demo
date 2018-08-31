0b2ca28d04d61111e51e695ff65a391029586e29
DEVOLO\Dimitar.Jilanov <djilanov@gmail.com>
2018-08-31 23:05:05 +0300

    Add address to order

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,23 +1,26 @@
 import { LocalstorageCartModel } from './localstorage-cart.model';
 
 export class OrderModel {
-	name: string;
-	email: string;
-	phone: string;
-    message: string;
-    products: LocalstorageCartModel[]
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+  address: string;
+  products: LocalstorageCartModel[]
 
-	constructor(
-		name?: string,
-		email?: string,
-		phone?: string,
-		message?: string,
-        products?: LocalstorageCartModel[]
-	) {
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.message = message;
-		this.products = products;
-	}
+  constructor(
+    name?: string,
+    email?: string,
+    phone?: string,
+    message?: string,
+    address?: string,
+    products?: LocalstorageCartModel[]
+  ) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.message = message;
+    this.address = address;
+    this.products = products;
+  }
 }
